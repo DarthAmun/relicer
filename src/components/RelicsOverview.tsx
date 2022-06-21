068d8ba10ad12c525206ceef6c43e28f1ab98208
DarthAmun <37881139+DarthAmun@users.noreply.github.com>
2022-06-21 17:44:09 +0200

    init

---
+++ src/components/RelicsOverview.tsx
@@ -0,0 +1,11 @@
+import Relic, { RelicType } from "./Relic";
+
+const RelicsOverview = () => {
+  return (
+    <>
+      <Relic stars={4} type={RelicType.AbidingPanacea} />
+    </>
+  );
+};
+
+export default RelicsOverview;
