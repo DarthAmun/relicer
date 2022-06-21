78088d00a943833f3b067152380c426fe04eb2e9
DarthAmun <37881139+DarthAmun@users.noreply.github.com>
2022-06-21 21:34:53 +0200

    relics and icons

--- src/components/Relic.tsx
+++ src/components/Relic.tsx
@@ -1,7 +1,26 @@
+import { FaQuestion } from "react-icons/fa";
 import styled from "styled-components";
-import { HammerThor } from "../icons/RelicIcons";
+import {
+  AbidingPanacea,
+  Adamantine,
+  ApollosBow,
+  AstralWitchcraft,
+  Enchanter,
+  HammerThor,
+  ImmensusPeak,
+  Incandescence,
+  LightAbove,
+  MasterGrove,
+  OceanWaves,
+  SnowDowager,
+  Stoneveins,
+  SwordAvatara,
+  TyrannyZeus,
+  WarMaschine,
+  WindWalker,
+} from "../icons/RelicIcons";
 
-export enum RelicType {
+export enum RelicCategory {
   WindWalker,
   WarMaschine,
   AbidingPanacea,
@@ -21,7 +40,7 @@ export enum RelicType {
   ApollosBow,
 }
 
-export enum PlaceType {
+export enum RelicPlace {
   Circle,
   Square,
   Triangle,
@@ -39,36 +58,197 @@ export interface Stat {
   bonus: number;
 }
 
-interface $RelicType {
+export interface $Relic {
   stars: number;
-  type: RelicType;
+  category: RelicCategory;
   plus: number;
-  place: PlaceType;
+  place: RelicPlace;
   mainStat: MainStat;
   subStats: Stat[];
 }
 
-const Relic = ({
-  stars,
-  type,
-  plus,
-  place,
-  mainStat,
-  subStats,
-}: $RelicType) => {
+interface $RelicType {
+  relic: $Relic;
+}
+
+const Relic = ({ relic }: $RelicType) => {
   const findIcon = () => {
-    return <HammerThor></HammerThor>;
+    switch (relic.category) {
+      case RelicCategory.AbidingPanacea:
+        return <AbidingPanacea />;
+      case RelicCategory.WindWalker:
+        return <WindWalker />;
+      case RelicCategory.WarMaschine:
+        return <WarMaschine />;
+      case RelicCategory.OceanWaves:
+        return <OceanWaves />;
+      case RelicCategory.HammerThor:
+        return <HammerThor />;
+      case RelicCategory.TyrannyZeus:
+        return <TyrannyZeus />;
+      case RelicCategory.SnowDowager:
+        return <SnowDowager />;
+      case RelicCategory.AstralWitchcraft:
+        return <AstralWitchcraft />;
+      case RelicCategory.MasterGrove:
+        return <MasterGrove />;
+      case RelicCategory.ImmensusPeak:
+        return <ImmensusPeak />;
+      case RelicCategory.Enchanter:
+        return <Enchanter />;
+      case RelicCategory.LightAbove:
+        return <LightAbove />;
+      case RelicCategory.SwordAvatara:
+        return <SwordAvatara />;
+      case RelicCategory.Incandescence:
+        return <Incandescence />;
+      case RelicCategory.Adamantine:
+        return <Adamantine />;
+      case RelicCategory.Stoneveins:
+        return <Stoneveins />;
+      case RelicCategory.ApollosBow:
+        return <ApollosBow />;
+      default:
+        <FaQuestion />;
+    }
   };
 
-  return <Wrapper stars={stars}>{findIcon()}</Wrapper>;
+  const findPlace = () => {
+    switch (relic.place) {
+      case RelicPlace.Circle:
+        return <Circle>{findIcon()}</Circle>;
+      case RelicPlace.Square:
+        return <Square>{findIcon()}</Square>;
+      case RelicPlace.Diamond:
+        return <Diamond>{findIcon()}</Diamond>;
+      case RelicPlace.Hexagon:
+        return <Hexagon>{findIcon()}</Hexagon>;
+      case RelicPlace.Spade:
+        return <Spade>{findIcon()}</Spade>;
+      case RelicPlace.Triangle:
+        return <Triangle>{findIcon()}</Triangle>;
+      default:
+        <FaQuestion />;
+    }
+  };
+
+  return <Wrapper stars={relic.stars}>{findPlace()}</Wrapper>;
 };
 
 export default Relic;
+
+const handleColorType = (stars: number) => {
+  switch (stars) {
+    case 5:
+      return "#eb6476";
+    case 4:
+      return "#EAB25D";
+    case 3:
+      return "MediumOrchid";
+    case 2:
+      return "lightgreen";
+    default:
+      return "#fff";
+  }
+};
 
 const Wrapper = styled.div<{ stars: number }>`
   height: 80px;
   width: 80px;
   border-bottom-right-radius: 10px;
   border-top-left-radius: 10px;
-  background-color: #eb6476;
+  background-color: ${({ stars }) => handleColorType(stars)};
+`;
+
+const Square = styled.div`
+  float: left;
+  margin: 10px;
+  padding: 10px;
+  height: 60px;
+  width: 60px;
+  background-color: purple;
+  & svg {
+    width: 40px;
+    fill: white;
+    z-index: 10;
+    position: relative;
+  }
+`;
+const Circle = styled(Square)`
+  border-radius: 40px;
+`;
+const Diamond = styled(Square)`
+  margin: 15px;
+  padding: 7px;
+  height: 50px;
+  width: 50px;
+  transform: rotate(45deg);
+  & svg {
+    transform: rotate(-45deg);
+  }
+`;
+const Hexagon = styled(Square)`
+  &:before {
+    content: " ";
+    width: 0;
+    height: 0;
+    border-bottom: 30px solid purple;
+    border-left: 52px solid transparent;
+    border-right: 52px solid transparent;
+    position: absolute;
+    top: -30px;
+    left: 0px;
+  }
+
+  width: 104px;
+  height: 60px;
+  margin-left: -12px;
+
+  &:after {
+    content: "";
+    width: 0;
+    position: absolute;
+    bottom: -30px;
+    left: 0px;
+    border-top: 30px solid purple;
+    border-left: 52px solid transparent;
+    border-right: 52px solid transparent;
+  }
+
+  transform: scale(0.6);
+  & svg {
+    margin-left: 20px;
+    transform: scale(1.6);
+  }
+`;
+const Spade = styled(Square)`
+  background-color: transparent;
+  position: relative;
+  &:before {
+    color: purple;
+    z-index: 0;
+    content: "♠";
+    font-size: 120px;
+    width: 60px;
+    height: 60px;
+    text-align: center;
+    line-height: 30px;
+    position: absolute;
+    left: 0;
+  }
+`;
+const Triangle = styled(Square)`
+  background-color: transparent;
+  position: relative;
+  width: 0;
+  height: 0;
+  padding: 0;
+  border-left: 30px solid transparent;
+  border-right: 30px solid transparent;
+  border-bottom: 60px solid purple;
+
+  & svg {
+    position: absolute;
+    left: 0px;
+  }
 `;
--- src/components/RelicsOverview.tsx
+++ src/components/RelicsOverview.tsx
@@ -1,11 +1,53 @@
-import Relic, { RelicType } from "./Relic";
+import styled from "styled-components";
+import Relic, { $Relic, MainStat, RelicCategory, RelicPlace } from "./Relic";
 
 const RelicsOverview = () => {
+  const getRandomArbitrary = (min: number, max: number) => {
+    return Math.round(Math.random() * (max - min) + min);
+  };
+
+  function randomEnum<T>(anEnum: T): T[keyof T] {
+    const enumValues = Object.keys(anEnum)
+      .map((n) => Number.parseInt(n))
+      .filter((n) => !Number.isNaN(n)) as unknown as T[keyof T][];
+    const randomIndex = Math.floor(Math.random() * enumValues.length);
+    const randomEnumValue = enumValues[randomIndex];
+    return randomEnumValue;
+  }
+
+  const generateRelics = (amount: number) => {
+    let relics: $Relic[] = [];
+    for (let i = 0; i < amount; i++) {
+      let newRelic: $Relic = {
+        stars: getRandomArbitrary(2, 5),
+        category: randomEnum(RelicCategory),
+        plus: getRandomArbitrary(0, 12),
+        place: randomEnum(RelicPlace),
+        mainStat: MainStat.Circle,
+        subStats: [],
+      };
+      relics.push(newRelic);
+    }
+    return relics;
+  };
+
+  const rels: $Relic[] = generateRelics(50);
+  console.log(rels);
+
   return (
-    <>
-      <Relic stars={4} type={RelicType.AbidingPanacea} />
-    </>
+    <Wrapper>
+      {rels.map((rel) => (
+        <Relic relic={rel} />
+      ))}
+    </Wrapper>
   );
 };
 
 export default RelicsOverview;
+
+const Wrapper = styled.div`
+  padding: 10px;
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+`;
